cb7990c452ffcdfcb186f36c02bed2413ba97ed5
Thom Lu <thom@befinch.com>
2025-04-12 00:03:13 -0400

    fix audio playback

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 8000
-    }
+        port: 8000,
+        cors: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    },
+    base: '/pokemon_music_player/',
+    assetsInclude: ['**/*.mp3']
 }) 
